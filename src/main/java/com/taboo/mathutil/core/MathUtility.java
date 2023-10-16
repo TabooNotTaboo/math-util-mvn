e9cfcc23e680ae5caf8b41c48a85e583001942cb
TabooNotTaboo <baolptse171084@fpt.edu.vn>
2023-10-16 11:26:30 +0700

    update getf() to use recursion and to see gree4ever

--- src/main/java/com/taboo/mathutil/core/MathUtility.java
+++ src/main/java/com/taboo/mathutil/core/MathUtility.java
@@ -10,7 +10,7 @@ package com.taboo.mathutil.core;
  */
 public class MathUtility {
    public static final double PI =3.14;
-   public static long getFactorial (int n) {
+      public static long getFactorial (int n) {
        //ham tinh n! = 1,2,3... n
        //0!= 1!=1 -quy uoc
        //khong tinh giai thua so am 
@@ -20,10 +20,23 @@ public class MathUtility {
            throw new IllegalArgumentException("Invalid n.n must be 0...20");
        if (n == 0 ||  n==1)
            return 1 ;
-       long result =10; 
-       for (int i = 2; i <= n; i++) {
-           result  *=i;
-       }
-       return result;
-   }
+       return n * getFactorial(n-1);
+      }
+       
+//   public static long getFactorial (int n) {
+//       //ham tinh n! = 1,2,3... n
+//       //0!= 1!=1 -quy uoc
+//       //khong tinh giai thua so am 
+//       //n! tang rat nhanh ,20! vua du full kieu long 18 con so 0
+//       //quy uoc <0 ! va > 20 NEM NGOAI LE, KHONG TINH DC
+//       if (n<0 || n>20 )
+//           throw new IllegalArgumentException("Invalid n.n must be 0...20");
+//       if (n == 0 ||  n==1)
+//           return 1 ;
+//       long result =1; 
+//       for (int i = 2; i <= n; i++) {
+//           result  *=i;
+//       }
+//       return result;
+//   }
 }
